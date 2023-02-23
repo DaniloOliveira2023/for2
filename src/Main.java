ade6216804aef53ffcdf6683697cf94f298b02ee
Danilo <oliveira.sport@hotmail.com>
2023-02-23 15:53:01 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int numero=100;numero<=400;numero++) {
+            if (numero%4==0) {
+                System.out.println(numero);
+            }
+        }
+    }
+}
